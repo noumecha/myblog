2f224323cae8a1bbd5912d272c8b1c09e7915786
noumecha spaker <noumechaivan@gmail.com>
2023-08-07 10:22:06 +0100

    upd : adding all tables for simples blog system

---
+++ app/Models/Categorie.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    use HasFactory;
+
+    /**
+     * @inheritdoc
+     */
+    public function getId() {
+        return $this->attributes['id'];
+    }
+    /**
+     * @inheritdoc
+     */
+    public function setId($id) {
+        $this->attributes['id'] = $id;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getName() {
+        return $this->attributes['name'];
+    }
+    /**
+     * @inheritdoc
+     */
+    public function setName($name) {
+        $this->attributes['name'] = $name;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getCreatedAt() {
+        return $this->attributes['created_at'];
+    }
+    /**
+     * @inheritdoc
+     */
+    public function setCreatedAt($createdAt) {
+        $this->attributes['created_at'] = $createdAt;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getUpdatedAt() {
+        return $this->attributes['updated_at'];
+    }
+    /**
+     * @inheritdoc
+     */
+    public function setUpdatedAt($updatedAt) {
+        $this->attributes['updated_at'] = $updatedAt;
+    }
+}
+
